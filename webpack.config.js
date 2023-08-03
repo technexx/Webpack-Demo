1211da03f2db1747dfd8eaf106c4cabc376728c3
Brian Salomone <brian.j.salomone@gmail.com>
2023-08-03 11:33:14 -0700

    added html plugin + cleaned up dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+module.exports = {  
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
-  },
+  },  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
     // filename: 'main.js',
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
-  mode: 'development',
 };
